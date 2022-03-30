c9e369650cc5461f442ebeec6db2dd88b9f7c704
Alex HUNAULT <alexx.hunault@gmail.com>
2022-03-30 18:02:25 +0200

    fonction getFav et searchFav

--- requests_server.js
+++ requests_server.js
@@ -21,7 +21,7 @@ async function addNewFav(meteo){
 }
 
 
-async function getFavoris(){
+async function getAllFavoris(){
 
     const settings = {
         method : "GET",
@@ -42,5 +42,71 @@ async function getFavoris(){
     else{
         const data = await response.json();
         console.log(data["message"]);
+    }
+}
+
+
+async function searchFav(query){
+
+    const settings = {
+        method : "GET",
+        headers : {
+            "Accept" : "application/json",
+            "Content-Type" : "application/json",
+        },
+    };
+
+    const response = await fetch(`http://localhost:3000/favoris?city_like=${query}`,settings);
+    if(response.ok){
+        const data = await response.json();
+        console.log(data);
+        return data;
+    }
+    else{
+        const data = await response.json();
+        console.log(data["message"]);
+    }
+}
+
+
+async function getFavoris(id){
+
+    const settings = {
+        method : "GET",
+        headers : {
+            "Accept" : "application/json",
+            "Content-Type" : "application/json",
+        },
+    };
+
+    const response = await fetch(`http://localhost:3000/favoris/${id}`,settings);
+    if(response.ok){
+        const data = await response.json();
+        console.log(data);
+        return data;
+    }
+    else{
+        const data = await response.json();
+        console.log(data["message"]);
+    }
+}
+
+
+
+async function suppFavoris(id){
+
+    const settings = {
+        method : "DELETE",
+        headers : {
+            "Accept" : "application/json",
+            "Content-Type" : "application/json",
+        },
+    };
+
+    const response = await fetch(`http://localhost:3000/favoris/${id}`,settings);
+    if(!response.ok){
+        const data = await response.json();
+        console.log(data);
+        return data;
     }
 }
